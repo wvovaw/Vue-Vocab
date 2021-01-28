71edfb23269b0c67dfbcb8233fe219470c74c2ac
Vladimir Ivakhno <vladimir5313375015@gmail.com>
2021-01-28 16:40:07 +0500

    Fixed AddWordDialog. Highlight empty fields

--- src/components/AddWordDialog.vue
+++ src/components/AddWordDialog.vue
@@ -1,28 +1,31 @@
 <template>
-  <div>
-    <!-- FIXME: Errors on ESC and out of border click -->
-    <md-dialog :md-active.sync="this.$parent.showDialog">
-      <md-dialog-title>Add word</md-dialog-title>
-      <form novalidate class="md-layout" @submit.prevent="saveData">
-        <md-field>
-          <label>Word</label>
-          <!-- TODO: Autofocus when dialog opens -->
-          <md-input ref="word" v-model="entry.wordPair.word"></md-input>
-        </md-field>
-        <md-field>
-          <label>Translation</label>
-          <md-input
-            ref="translation"
-            v-model="entry.wordPair.translation"
-          ></md-input>
-        </md-field>
-        <md-dialog-actions>
-          <md-button class="md-primary" @click="closeDialog">Cancel</md-button>
-          <md-button type="submit" class="md-primary">Save</md-button>
-        </md-dialog-actions>
-      </form>
-    </md-dialog>
-  </div>
+  <md-dialog
+    :md-active.sync="this.$parent.showDialog"
+    :md-click-outside-to-close="false"
+    :md-close-on-esc="false"
+    :md-fullscreen="false"
+  >
+    <md-dialog-title>Add word</md-dialog-title>
+    <form novalidate class="md-layout" @submit.prevent="saveData">
+      <md-field :class="isInvalid(emptyWord)">
+        <label>Word</label>
+        <!-- TODO: Autofocus when dialog opens -->
+        <md-input ref="word" v-model="entry.wordPair.word" required></md-input>
+      </md-field>
+      <md-field :class="isInvalid(emptyTranslation)">
+        <label>Translation</label>
+        <md-input
+          ref="translation"
+          v-model="entry.wordPair.translation"
+          required
+        ></md-input>
+      </md-field>
+      <md-dialog-actions>
+        <md-button class="md-primary" @click="closeDialog">Cancel</md-button>
+        <md-button type="submit" class="md-primary">Save</md-button>
+      </md-dialog-actions>
+    </form>
+  </md-dialog>
 </template>
 
 <script>
@@ -38,16 +41,32 @@ export default {
           word: "",
           translation: ""
         }
-      }
+      },
+      emptyWord: false,
+      emptyTranslation: false
     };
   },
   methods: {
+    isInvalid(invalidFlag) {
+      return {
+        "md-invalid": invalidFlag
+      };
+    },
     closeDialog() {
       this.$emit("toggleDialog");
+      // Clean fields
+      this.entry = {
+        id: null,
+        wordPair: {
+          word: "",
+          translation: ""
+        }
+      };
+      this.emptyWord = this.emptyTranslation = false;
     },
     saveData() {
       if (this.entry.wordPair.word && this.entry.wordPair.translation) {
-        // Capitalize 1st letters
+        // Capitalize 1st letters, decapitalize the rest
         this.entry.wordPair.word =
           this.entry.wordPair.word.charAt(0).toUpperCase() +
           this.entry.wordPair.word.slice(1).toLowerCase();
@@ -56,17 +75,15 @@ export default {
           this.entry.wordPair.translation.charAt(0).toUpperCase() +
           this.entry.wordPair.translation.slice(1).toLowerCase();
 
+        // Add the entry in dictionary
         this.$store.dispatch("addWord", this.entry);
+        // Close dialog
+        this.closeDialog();
+      } else {
+        // Else highlight empty fields
+        if (!this.entry.wordPair.word) this.emptyWord = true;
+        if (!this.entry.wordPair.translation) this.emptyTranslation = true;
       }
-      // TODO: Else Highlight empty fields etc.
-      this.entry = {
-        id: null,
-        wordPair: {
-          word: "",
-          translation: ""
-        }
-      };
-      this.$emit("toggleDialog");
     }
   },
   destroyed() {
